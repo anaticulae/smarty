53ee6fbb185af5ef462d56c834c4ab567c6ae6e3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-02 16:05:16 +0100

    feat(bad): add list with fat words

---
+++ smarty/badwords/fat.py
@@ -0,0 +1,252 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+FAT = """\
+aber
+abermals
+allein
+allem Anschein nach
+allemal
+allenfalls
+allenthalben
+allerallerdings
+allerdings
+allesamt
+allzu
+also
+an sich
+andauernd
+anderenfalls
+andererseits
+anscheinend
+auch
+auffallend
+aufs Neue
+augenscheinlich
+ausdrücklich
+ausgerechnet
+ausnahmslos
+außerdem
+bei weitem
+beinahe
+bekanntlich
+bereits
+besonders
+bestenfalls
+bestimmt
+bloß
+dabei
+dadurch
+dafür
+dagegen
+daher
+damals
+danach
+dann und wann
+demgegenüber
+demgemäß
+demnach
+denkbar
+denn
+dennoch
+des Öfteren
+deshalb
+desungeachtet
+deswegen
+doch
+durchaus
+durchweg
+eben
+eigentlich
+ein bisschen
+ein wenig
+einerseits
+einfach
+eingermaßen
+einige
+einmal
+ergo
+erheblich
+etliche
+etwa
+etwas
+fast
+folgendermaßen
+folglich
+fortwährend
+fraglos
+freilich
+förmlich
+ganz
+ganz und gar
+gar
+gelegentlich
+gemeinhin
+genau
+geradezu
+gewiss
+gewissermaßen
+glatt
+gleichsam
+gleichwohl
+glücklicherweise
+gottseidank
+größtenteils
+gänzlich
+halt
+hier und da
+hingegen
+hinlänglich
+häufig
+höchst
+höchstens
+im Allgemeinen
+immer
+immerhin
+immerzu
+in der Tat
+in diesem Zusammenhang
+indessen
+infolgedessen
+insbesondere
+inzwischen
+irgend
+irgendein
+irgendjemand
+irgendwann
+irgendwie
+irgendwo
+ja
+je
+jedenfalls
+jedoch
+jemals
+kaum
+keinesfalls
+keineswegs
+lediglich
+leider
+letzlich
+längst
+manchmal
+mehr oder weniger
+mehrfach
+meines Erachtens
+meinetwegen
+meist
+meistens
+meistenteils
+mindestens
+mithin
+mitunter
+möglicherweise
+möglichst
+naturgemäß
+natürlich
+neuerdings
+neuerlich
+neulich
+nichtsdestoweniger
+nie
+niemals
+nun
+nur
+nämlich
+offenbar
+offenkundig
+offensichtlich
+oft
+ohne Zweifel
+ohne weieres
+ohnedies
+partout
+plötzlich
+praktisch
+quasi
+recht
+reichlich
+reiflich
+relativ
+restlos
+richtiggehend
+rundheraus
+rundum
+samt und sonders
+sattsam
+schlicht
+schlichtweg
+schließlich
+schlussendlich
+schon
+sehr
+selbst
+selbstredend
+selbstverständlich
+selten
+seltsamerweise
+sicher
+sicherlich
+so
+sogar
+sonst
+sowieso
+sowohl als auch
+sozusagen
+stellenweise
+stets
+trotzdem
+umständehalber
+unbedingt
+unerhört
+ungefähr
+ungemein
+ungewöhnlich
+ungleich
+unglücklicherweise
+unlängst
+unmaßgeblich
+unsagbar
+unstreitig
+unsäglich
+unzweifelhaft
+vergleichsweise
+vermutlich
+vielfach
+vielleicht
+voll
+voll und ganz
+vollends
+vollkommen
+vollständig
+von neuem
+völlig
+wahrscheinlich
+weidlich
+weitgehend
+wenigstens
+wieder
+wiederum
+wohl
+wohlgemerkt
+womöglich
+ziemlich
+zudem
+zugegeben
+zumeist
+zusehends
+zuweilen
+zweifellos
+zweifelsfrei
+zweifelsohne
+äußerst
+überaus
+überdies
+überhaupt
+übrigens
+"""
