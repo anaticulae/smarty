7f8d677adc070956b1815e9396564fe0cd591a43
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-08 14:27:16 +0100

    feat(badword): add first draft of word improvement feature

---
+++ smarty/badwords/improvement.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""Improvement
+===========
+
+The Improvement detector detects a group of words and give an
+replacement to the user.
+There can be a optional description why this improvement is required.
+"""
+
+IMPROVEMENT = """\
+höchste Exaktheit
+höchste Genauigkeit
+TODO
+
+"""
