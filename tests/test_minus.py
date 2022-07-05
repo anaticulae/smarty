0056de2805fc6f6d6f3b80bd16d9062572f292aa
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-07-06 00:38:55 +0200

    chore(requirements): upgrade requirements.txt
    + adjust unit test after adding percent to konrad

--- tests/test_minus.py
+++ tests/test_minus.py
@@ -39,4 +39,4 @@ def test_cli_spelling(testdir, monkeypatch):
     path = smarty.path.smarty_spelling_hyphen(testdir.tmpdir)
     loaded = serializeraw.load_textadvices(path)
     assert loaded
-    assert len(loaded) >= 58
+    assert len(loaded) >= 56
